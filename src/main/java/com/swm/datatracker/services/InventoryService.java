4c79944411ad18539ace0e18bb5b35582709cd19
jameskevinayoung <james.kevin.a.young@gmail.com>
2018-11-08 11:05:07 -0600

    updated repo and service

--- src/main/java/com/swm/datatracker/services/InventoryService.java
+++ src/main/java/com/swm/datatracker/services/InventoryService.java
@@ -4,6 +4,8 @@ package com.swm.datatracker.services;
 import com.swm.datatracker.models.Inventory;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class InventoryService {
 
@@ -13,12 +15,20 @@ public class InventoryService {
         this.inventoryRepo = inventoryRepo;
     }
 
-//--------------------- METHODS TO BE USED IN CONTROLLER -------------------------------\\
+
+
+
+//------------------------------- METHODS TO BE USED IN CONTROLLER -------------------------------\\
+
+
 
 //FINDS ALL ITEMS IN THE DATABASE
     public Iterable<Inventory> all(){
         return inventoryRepo.findAll();
     }
+
+
+//--------------------- UPDATES ---------------------\\
 
 //CREATES A NEW INVENTORY OBJECT (ROW) IN THE DATABASE
     public Inventory create(Inventory item){
@@ -34,4 +44,21 @@ public class InventoryService {
     public void delete(Inventory inventory){
         inventoryRepo.delete(inventory);
     }
+
+
+
+//--------------------- SEARCHES ---------------------\\
+
+//SEARCHES DATABASE FOR INVENTORY BASED ON NAME ONLY
+    public List<Inventory> searchDBName(String term){
+        return inventoryRepo.findInventoryByNameContains(term);
+    }
+
+//SEARCHES DATABASE FOR INVENTORY BASED ON NAME OR SIZE
+    public List<Inventory> searchDBNameOrSize(String term1, String term2){
+        return inventoryRepo.findInventoryByNameContainsOrSizeContains(term1, term2);
+    }
+
+
+
 }

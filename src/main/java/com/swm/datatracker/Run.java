0b4cd5f5d50bebea4bda53209a01e4e2ff653295
RyanMStewart <ryan86stewart@gmail.com>
2018-11-15 14:05:29 -0600

    Updated application entry point

--- src/main/java/com/swm/datatracker/Run.java
+++ src/main/java/com/swm/datatracker/Run.java
@@ -3,11 +3,11 @@ package com.swm.datatracker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class Run {
-
-    public static void main(String[] args) {
-        SpringApplication.run(Run.class, args);
-    }
-
-}
+//@SpringBootApplication
+//public class Run {
+//
+//    public static void main(String[] args) {
+//        SpringApplication.run(Run.class, args);
+//    }
+//
+//}

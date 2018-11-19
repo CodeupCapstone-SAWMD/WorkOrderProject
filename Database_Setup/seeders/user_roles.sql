0132db79bd8fe10d1851f219f252f6561a50fb57
jameskevinayoung <james.kevin.a.young@gmail.com>
2018-11-19 09:13:09 -0600

    trying to correct my database errors

--- Database_Setup/seeders/user_roles.sql
+++ Database_Setup/seeders/user_roles.sql
@@ -1,3 +1,3 @@
 insert into user_roles (id, user_roles) values (1, 'ADMINISTRATOR');
-insert into user_roles (id, user_roles) values (2, 'EMPLOYEE');
-insert into user_roles (id, user_roles) values (3, 'USER');
+insert into user_roles (id, user_roles) values (2, 'USER');
+insert into user_roles (id, user_roles) values (3, 'EMPLOYEE');

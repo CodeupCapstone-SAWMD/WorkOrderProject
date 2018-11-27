43ce3096c15efc3fc9113055a17da5bc39579c6a
jameskevinayoung <james.kevin.a.young@gmail.com>
2018-11-27 15:40:47 -0600

    updated the seeders in the repo

--- Database_Setup/seeders/status.sql
+++ Database_Setup/seeders/status.sql
@@ -1,8 +1,5 @@
-use mgmt_db;
-
 insert into status (id, name) values (1, 'SUBMITTED');
-insert into status (id, name) values (2, 'PENDING ASSIGNMENT');
-insert into status (id, name) values (3, 'PROCESSING');
-insert into status (id, name) values (4, 'REVIEW');
-insert into status (id, name) values (5, 'COMPLETE');
-insert into status (id, name) values (6, 'CANCELED');
+insert into status (id, name) values (2, 'PROCESSING');
+insert into status (id, name) values (3, 'REVIEW');
+insert into status (id, name) values (4, 'COMPLETE');
+insert into status (id, name) values (5, 'CANCELED');
--- Database_Setup/seeders/user_roles.sql
+++ Database_Setup/seeders/user_roles.sql
@@ -1,3 +1,9 @@
-insert into user_roles (id, user_roles) values (1, 'ADMINISTRATOR');
-insert into user_roles (id, user_roles) values (2, 'USER');
-insert into user_roles (id, user_roles) values (3, 'EMPLOYEE');
+insert into user_roles (id, user_roles, user_id) values (1, 'ROLE_ADMIN', 1);
+insert into user_roles (id, user_roles, user_id) values (2, 'ROLE_USER', 2);
+insert into user_roles (id, user_roles, user_id) values (3, 'ROLE_EDITOR', 3);
+insert into user_roles (id, user_roles, user_id) values (4, 'ROLE_EDITOR', 3);
+insert into user_roles (id, user_roles, user_id) values (5, 'ROLE_USER', 2);
+insert into user_roles (id, user_roles, user_id) values (6, 'ROLE_USER', 2);
+insert into user_roles (id, user_roles, user_id) values (7, 'ROLE_EDITOR', 3);
+insert into user_roles (id, user_roles, user_id) values (8, 'ROLE_USER',2);
+insert into user_roles (id, user_roles, user_id) values (9, 'ROLE_EDITOR', 3);
